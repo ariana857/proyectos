36aae5b56cf1d5ae0c8265844d91faccbb15619a
ariana857 <arianaolalde148@gmail.com>
2024-05-16 15:16:14 -0700

    Add files via upload

---
+++ conexionp.php
@@ -0,0 +1,11 @@
+<?php
+	
+  $conexion=mysqli_connect("localhost","root","","panaderia");
+  if(!$conexion){
+  	echo "No se pudo conectar a la base de datos";
+   }
+ /* }else{
+  	echo "La base de datos sea conectado correctamente";
+  }*/
+   
+?>
